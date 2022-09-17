30e67e0d928090868b1ed5b13d86af1712c62505
EliasGalindo0 <eliasgalindo@gmail.com>
2022-09-17 16:22:03 -0300

    feat: readOne motorcycles

--- src/controllers/MotorcycleController.ts
+++ src/controllers/MotorcycleController.ts
@@ -13,10 +13,17 @@ class MotorcycleController {
     res.status(201).json(created);
   }
 
-  public async read(req: Request, res: Response) {
+  public async read(_req: Request, res: Response) {
     const motorcycles = await this._service.read();
 
     return res.status(200).json(motorcycles);
+  }
+
+  public async readOne(req: Request, res: Response) {
+    const { id } = req.params;
+    const motorcycle = await this._service.readOne(id);
+
+    return res.status(200).json(motorcycle);
   }
 }
 
